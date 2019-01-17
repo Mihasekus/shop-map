4ada94cd6d7ba7c2db83e752c0556bde83c445d5
MPeshko <mihalilusoid@gmail.com>
2019-01-18 00:44:13 +0300

    added types

---
+++ src/app/models/address.ts
@@ -0,0 +1,19 @@
+class Marker {
+  lat: number;
+  lng: number;
+  label?: string;
+  draggable: boolean;
+}
+
+class Location {
+  lat: number;
+  lng: number;
+  viewport?: Object;
+  zoom: number;
+  address_level_1?:string;
+  address_level_2?: string;
+  address_country?: string;
+  address_zip?: string;
+  address_state?: string;
+  marker?: Marker;
+}
